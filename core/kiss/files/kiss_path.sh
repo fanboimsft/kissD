c4b6c8c05bb0ff4187ba911a6f8cddff5ebce79b
fanboimsft <fanboimsft>
2021-01-05 18:48:29 +0700

    Revert "link core-kiss packages with kiss-link/ fixes"
    This reverts commit 5684226e2df2814cb135ee969c445e1203a653af.

--- core/kiss/files/kiss_path.sh
+++ core/kiss/files/kiss_path.sh
@@ -1,1 +1,1 @@
-export KISS_PATH=/repo/kissD/core:/repo/kissD/core-kiss:/repo/kissD/extra:/repo/kiss/core:/repo/kiss/extra:/repo/kiss/xorg:/repo/kissD/bloat:/repo/community/community
+export KISS_PATH=/repo/kissD/core:/repo/kissD/extra:/repo/kiss/core:/repo/kiss/extra:/repo/kiss/xorg:/repo/community/community
